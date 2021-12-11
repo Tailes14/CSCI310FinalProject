21d5b1bf913c1410724c78054a1c9225cbc32387
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2021-12-10 20:18:27 -0500

    worksish using promises

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -5,10 +5,32 @@ import './App.css';
 import React, {useState, useEffect} from 'react'
 import {getAccessToken} from './kroger/krogerToken'
 
+async function getProductKroger(term, setResults) 
+{
+  let data = await getAccessToken()
+  let response = await fetch('https://api-ce.kroger.com/v1/products?filter.term=' + term, {
+    method : 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization' : 'Bearer ' + data.access_token
+    }
+  }).then(function(res) {
+    console.log(res)
+  })
+  setResults(response)
+  return response
+}
+
+
 
 function App() {
-  
-  let data = getAccessToken()
+  const [results, setResults] = useState()
+  const handleClick = () => {
+    let temp = getProductKroger('beer', setResults).then(result => {
+      console.log(result)
+    })
+    console.log(results)
+  }
   return (
     <div className="App">
 
@@ -24,6 +46,7 @@ function App() {
            <div className = "landingLHS">
               <h1 className = "landingHeader">We Want to Find <br/>Cheap Beer For You </h1>
               <p className = "landingP">At the Cheap Canteen, we only want the cheapest and  <br/>most affordable options for our users. We show the hottest <br/> beer by known retailers and give you the cheapest options!  </p>
+              <button onClick={handleClick}>Test</button>
             </div>
       </div>
     </div>
--- final-project/src/kroger/krogerToken.js
+++ final-project/src/kroger/krogerToken.js
@@ -6,9 +6,22 @@ const kroger_clientSecret = "8I7o6r7H-ASawmJzjCKGdQm97NBg7vG9qD-iQDr2"
 const encoded = "Basic ZmluYWwtcHJvamVjdC02OTIxYjNhYWE0MDNmYTQxZmY3N2EzMWI5OWMzYWY3MzQ5ODQ2MDI5NzU2NDU3NjQyMjM6OEk3bzZyN0gtQVNhd21KempDS0dkUW05N05CZzd2RzlxRC1pUURyMg=="
 
 
+/*
+curl -X GET \
+  'https://api-ce.kroger.com/v1/products?filter.term=beer' \
+  -H 'Accept: application/json' \
+  -H 'Authorization: Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLWNlLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoidnl6bG52Y3dSUUZyRzZkWDBzU1pEQT09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJmaW5hbC1wcm9qZWN0LTY5MjFiM2FhYTQwM2ZhNDFmZjc3YTMxYjk5YzNhZjczNDk4NDYwMjk3NTY0NTc2NDIyMyIsImV4cCI6MTYzOTE4NTQ0NCwiaWF0IjoxNjM5MTgzNjM5LCJpc3MiOiJhcGktY2Uua3JvZ2VyLmNvbSIsInN1YiI6IjU0MjI4ZWRjLWQzYmEtNTBhMi04MGEyLWU4ODMzMjdkM2NmYSIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjM5MTgzNjQ0NjkzOTI3Njk3LCJhenAiOiJmaW5hbC1wcm9qZWN0LTY5MjFiM2FhYTQwM2ZhNDFmZjc3YTMxYjk5YzNhZjczNDk4NDYwMjk3NTY0NTc2NDIyMyJ9.ka9fl8WnoZbcntAndNRRpnIPTeVWXxDB4MST4iFGXZV6OdS9znK9hKRg53kfUr1iggdbBJ44L_GSHVUZLaOxruJWdGytedVWy9gdghki4QpyH-PE487OGJexcs8WNA9cZa10-hngXidd6R33So9qjweMwVyHg5Uqjf1HTGN2Sg58ezTnYgq9XhE8KTPmHU9dPP3yBndD_2TKGmfgQcDIUL0wE1rAHoJ-avSFmcYmS2Wf9F7zkx1IbkoeX2lOEASkBsD18S-8vtwf3BsFUziJ1QUBnrM5I_EIs-YlA0L67IRct9CCXmRftMxTt7sPMVzSNY_3-5kaCFPoLwxIXkofHQ'
+
+  curl -X POST \
+  'https://api-ce.kroger.com/v1/connect/oauth2/token' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Authorization: Basic ZmluYWwtcHJvamVjdC02OTIxYjNhYWE0MDNmYTQxZmY3N2EzMWI5OWMzYWY3MzQ5ODQ2MDI5NzU2NDU3NjQyMjM6OEk3bzZyN0gtQVNhd21KempDS0dkUW05N05CZzd2RzlxRC1pUURyMg==' \
+  -d 'grant_type=client_credentials&scope=product.compact'
+*/
+
 export async function getAccessToken() {
 
-  let tokenReponse = fetch('https://api-ce.kroger.com/v1/connect/oauth2/token', {
+  let tokenReponse = await fetch('https://api-ce.kroger.com/v1/connect/oauth2/token', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -18,8 +31,7 @@ export async function getAccessToken() {
 }).then(function(res) {
   return res.json()
 }).then(function(resJson) {
-  console.log(resJson)
   return resJson
 })
-
+  return tokenReponse
 }
